70c129d75b2cd30a1707a46d58e26584d317c722
Valeria Mora <77369085+ValeriaMora1011@users.noreply.github.com>
2021-06-24 13:36:11 -0500

    Add files via upload

---
+++ Modulo1/Programming/01_Projects/routers/index.js
@@ -0,0 +1,17 @@
+//traemos el paquete 
+const { Router } = require('express');
+
+//Inicializamos Router
+const router = Router()
+
+//Creacion de los edpoint
+
+
+// Exportamos las rutas
+module.exports.RouterIndex = router
+
+// module = {
+//     export: {
+//         RouterIndex: router
+//     }
+// }
