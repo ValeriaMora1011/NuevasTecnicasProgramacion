93b39aaa132adffc51acf073071b442fbc656dfa
Valeria Mora <77369085+ValeriaMora1011@users.noreply.github.com>
2021-06-25 15:51:11 -0500

    Add files via upload

--- Modulo1/Programming/01_Projects/routers/index.js
+++ Modulo1/Programming/01_Projects/routers/index.js
@@ -1,14 +1,28 @@
 //traemos el paquete 
-const { Router } = require('express');
+const { Router } = require("express");
 
 //Inicializamos Router
-const router = Router()
+const router = Router();
 
 //Creacion de los edpoint
-
-
+router
+    .get("/", (req, res) => {
+        res.send("Hola mundo con ExpressJs");
+    })
+    .get("/saludo", (req, res) => {
+        const { query } = req;
+        res.json({
+            saludo: `Hola soy ${query.nombre} ${query.apellido}`,
+        });
+    })
+    .get("/saludo/:nombre", (req, res) => {
+        const { params } = req;
+        res.json({
+            nombre: params.nombre,
+        });
+    });
 // Exportamos las rutas
-module.exports.RouterIndex = router
+module.exports.RouterIndex = router;
 
 // module = {
 //     export: {
