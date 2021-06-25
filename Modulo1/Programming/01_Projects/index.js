051b3dc996bf7da194910338416c3d36a7162970
Valeria Mora <77369085+ValeriaMora1011@users.noreply.github.com>
2021-06-25 15:50:40 -0500

    Add files via upload

--- Modulo1/Programming/01_Projects/index.js
+++ Modulo1/Programming/01_Projects/index.js
@@ -1,65 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 // genera una aplicacion express
 //Creacion del servidor
-const app = express()
+const app = express();
 
-//EndPoint
-app.get('/', (req, res) => {
-    res.send("Hola mundo con ExpressJs")
-});
+// Routes
+const { RouterIndex } = require('./routes/index');
 
-app.get("/saludo", (req, res) => {
-    //req.query
-    //console.log(req.query);
+app.use("/", RouterIndex);
+// app.use("/user", RouterIndex);
+// app.use("/user/data", RouterIndex);
 
+/*
+    "/" + "/" = "//" => "/"
+    "/" + "/saludo" = "//saludo" => "/saludo" 
+    "/" + "/saludo/:nombre" = "//saludo/:nombre" => "/saludo/:nombre"
 
-    //Desestructuracion de objetos
-    //Obtenemos la query del objeto request
-
-    const { 
-        query: { nombre, apellido },
-     } = req;
-
-
-    //Otra forma de obtener los datos
-    // const nombre = req.query.nombre
-    // const apellido = req.query.apellido
-
-
-    //Ejemplo
-    //--------------------
-    // const user = {
-    //     nombre "Val",
-    //     apellido = "Mora"
-    // }
-    // user.nombre
-    // user.apellido
-    // req.query.nombre
-    //---------------------
-
-    res.json({
-        saludo: `Hola soy ${query.nombre} ${query.apellido}`,
-    });
-});
-
-app.get("/saludo/:nombre", (req, res) => {
-    const { 
-        params: { nombre },
-    } = req; //Desestructamos nombre de params
-
-    //const { params } = req;
-    //res.json({
-    //    nombre: params.nombre,
-   //});
-
-    //console.log(req.params);
-    //const nombre = req.params.nombre
-
-    res.json({
-        nombre: nombre,
-    });
-});
+    "/user" + "/" = "/user/"
+    "/user" + "/saludo" = "/user/saludo"
+    "/user/data" + "/saludo" = "/user/data/saludo"
+*/
 
 //Levanta servidor
 app.listen(3000, () => { //Da vida o levanta el servidor app.listen
