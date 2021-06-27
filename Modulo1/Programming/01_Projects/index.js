e8fc1f36d269aeb473caf35405e319038f8d28d3
Valeria Mora <77369085+ValeriaMora1011@users.noreply.github.com>
2021-06-27 12:07:54 -0500

    Add files via upload

--- Modulo1/Programming/01_Projects/index.js
+++ Modulo1/Programming/01_Projects/index.js
@@ -1,11 +1,39 @@
-const express = require("express");
+//Creacion del servidor
+const express = require("express");      
+
+const app = express();                                             //Genera una aplicacion express
+
+const { RouterIndex } = require('./routes2/index');                 //Routes
+
+app.use("/", RouterIndex);
+
+app.listen(3000, () => {                                           //Levanta servidor
+    console.log("Servidor escuchando en http://localhost:3000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const express = require("express");
 
 // genera una aplicacion express
 //Creacion del servidor
 const app = express();
 
 // Routes
-const { RouterIndex } = require('./routes/index');
+const { RouterIndex } = require('./routes2/index');
 
 app.use("/", RouterIndex);
 // app.use("/user", RouterIndex);
@@ -19,9 +47,9 @@ app.use("/", RouterIndex);
     "/user" + "/" = "/user/"
     "/user" + "/saludo" = "/user/saludo"
     "/user/data" + "/saludo" = "/user/data/saludo"
-*/
+*
 
 //Levanta servidor
 app.listen(3000, () => { //Da vida o levanta el servidor app.listen
     console.log("Servidor escuchando en http://localhost:3000");
-});
+}); */
